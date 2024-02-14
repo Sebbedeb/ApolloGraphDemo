d4f477083dbca9cc6c36f3740423f64926ad8116
Sebbedeb <zor.seb.0901@gmail.com>
2024-02-14 22:31:56 +0100

    laver mutations

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,23 +1,10 @@
-import { persons } from "../routes/data";}
+import { persons } from "../routes/data";
 const QueryResolvers = {
     Query: {
         hello: () => "Hello world!",
 
         persons: () => persons,
-        persons: (_parent: never, args: { id: string }, _context: never, _info: never) => {
+        person: (_parent: never, args: { id: string }, _context: never, _info: never) => {
             return persons.find((person) => person.id === args.id);
-        }
-
-        users: () => users,
-        user: (_parent: never, args: { id: string }, _context: never, _info: never) => {
-            return users.find((user) => user.id === args.id);
-        },
-        book: (_parent: never, args: { id: string }, _context: never, _info: never) => {
-            return books.find((book) => book.id === args.id);
-        },
-        books: () => books,
-        category: (_parent: never, args: { id: string }, _context: never, _info: never) => {
-            return categories.find((category) => category.id === args.id);
-        },
-        categories: () => categories,
     }
+}}
--- src/routes/data.ts
+++ src/routes/data.ts
@@ -1,28 +1,28 @@
-const persons = {
-    "1": {
+const persons = [
+    {
         id: "1",
         name: "John",
         email: "john@john.com",
         age: 25
     },
-    "2": {
+    {
         id: "2",
         name: "Jane",
         email: "Jane@Jane.com",
         age: 30
     },
-    "3": {
+    {
         id: "3",
         name: "Doe",
         email: "Doe@Doe.com",
         age: 35
     },
-    "4": {
+    {
         id: "4",
         name: "Smith",
         email: "Smith@Smith.com",
         age: 40
     }
-}
+];
 
 export { persons };
